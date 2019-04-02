f40b2eb30fbbe9a0381368f9d906aaab8f484f62
natethegreat34 <43049313+natethegreat34@users.noreply.github.com>
2019-04-02 09:58:37 -0400

    Works but slow

--- Radix.java
+++ Radix.java
@@ -1,10 +1,12 @@
 import java.util.*;
 public class Radix{
-    private MyLinkedList[] pockets = new MyLinkedList[20];
-    private int [] data;
-    private int num;
+    private static MyLinkedList <Integer> [] pockets = new MyLinkedList [20];
+    private static int o;
+    private static int [] data;
+    private static int num;
     public static void radixsort(int[]datl){
-      if (data.length == 0){
+    // System.out.println(pockets.length);
+      if (datl.length == 0){
         return ;
       }
       num = maxd(datl);
@@ -15,34 +17,40 @@ public class Radix{
         radhelper (data, 0);
     }
     public static void radhelper(int [] data, int c){
-      if (c > num){
-        return;
-      }
+        // System.out.println(c + "it is more cs");
+      if (c <= num){
+    int current = (int) Math.pow(10,c);
         for (int i = 0; i < data.length; i ++){
             if(data[i] < 0){
                 //num is how many times should be divided by 10
                 // might need to take into account the negative sign
-                pockets[9 - (-1 *(data[i] / (Math.pow(10,i) % 10)))].add(data [i]);
-
-
+                pockets[9 + ((data[i] / current) % 10)].add(data [i]);
             }
             else {
-                pockets[10 + (data[i] / (Math.pow(10,i) % 10))].add(data [i]);
+                pockets[10 + ((data[i] / current) % 10)].add(data [i]);
+
             }
     }
-        reset (data);
-        radhelper(data, c++);''
+    // System.out.println(c + "  its a c" + o ++);
+        reset (data, c);
     }
-    public static void reset (int [] data){
+}
+    public static void reset (int [] data, int c){
       int y = 0;
-      for (int i = 0; i < pockets.length < i ++){
+      for (int i = 0; i < pockets.length ; i ++){
         while (pockets[i].size()> 0){
           data [y] = pockets[i].removeFront();
           y ++;
         }
       }
+      // System.out.println(c ++);
+
+
+
+      radhelper(data, c+1);
+      // System.out.println("k");
     }
-    private int maxd(int [] datl){
+    private static int maxd(int [] datl){
       int m = datl [0];
       for (int i = 1; i < datl.length; i ++){
         if (m < datl [i]){
